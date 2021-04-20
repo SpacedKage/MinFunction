25006391f2f332ab8068401474eba8feaf995a20
SpacedKage <74511080+SpacedKage@users.noreply.github.com>
2021-04-20 11:40:12 +0100

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+alert("A STRNGS Programme")
+
+console.log(alert)
+
+console.log(Math.min(2, 3, 1));
+// expected output: 1
+
+console.log(Math.min(-2, -3, -1));
+// expected output: -3
+
+const array1 = [20, 12, -85];
+
+console.log(Math.min(...array1));
+// expected output: -85
